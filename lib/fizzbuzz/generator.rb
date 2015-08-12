678771a22f029686f3cb0c4a4d68f6790c327dc7
joansenent <joansenent@hotmail.com>
2015-08-12 21:20:56 +0200

    fizzbuzz excercise done

--- lib/fizzbuzz/generator.rb
+++ lib/fizzbuzz/generator.rb
@@ -5,7 +5,41 @@ module FizzBuzz
 	class Generator
 		# This method returns a fizzbuzz list
 		def create(size)
-			Array.new(size)
+			(1..size).collect do |num|
+				if (num % 3 == 0) & (num % 5 == 0)
+					"fizzbuzz"
+				else
+					if (num % 3 == 0)
+						"fizz"
+					else
+						if (num % 5 == 0)
+							"buzz"
+						else
+							num
+						end
+					end
+				end
+			end
+
+			# n = 2
+			# while n < size
+			# 	fizzbuzz_array[n] << "fizz"
+			# 	n = n + 3
+			# end
+				
+				# if n % 3 == 0
+				# 	fizzbuzz_array(n) = "fizz"
+				# end
+			# n = 0
+			# while n < size
+			# 	if (n % 3 != 0) && (n % 5 != 0)
+			# 		fizzbuzz_array << n
+			# 	end
+			# 	if (n % 3 == 0) && (n % 5 == 0)
+
+			# 	end
+				
+			# end
 		end
 	end
 end
--- test/fizzbuzz/test_generator.rb
+++ test/fizzbuzz/test_generator.rb
@@ -1,7 +1,7 @@
 require_relative '../test_helper'
 
 # These tests test the Generator class
-class TestGame < Minitest::Test
+class TestGenerator < Minitest::Test
 	# This method runs once before each test
 	# It sets an instance member variable that can be used in the tests
 	def setup
@@ -17,8 +17,7 @@ class TestGame < Minitest::Test
     assert_equal(100, result.size)
   end
 
-	def test_numbers_that_are_not_substituted
-    skip
+	def test_numbers_that_are_not_substituted 
 		result = @generator.create(10)
 		assert_equal(1, result[0])
     assert_equal(2, result[1])
@@ -26,17 +25,29 @@ class TestGame < Minitest::Test
   end
 
   def test_numbers_that_are_divisible_by_3
-    skip
     result = @generator.create(100)
-    
+    assert_equal("fizz", result[2])
+    assert_equal("fizz", result[5])
+    assert_equal("fizz", result[8])
+    assert_equal("fizz", result[11])
+    assert_equal("fizz", result[98])
   end
 
   def test_numbers_that_are_divisible_by_5
-    skip
+    result = @generator.create(100)
+    assert_equal("buzz", result[4])
+    assert_equal("buzz", result[9])
+    assert_equal("buzz", result[49])
+    assert_equal("buzz", result[94])
+    assert_equal("buzz", result[99])
   end
 
   def test_numbers_that_are_divisible_by_3_and_5
-    skip
+    result = @generator.create(100)
+    assert_equal("fizzbuzz", result[14])
+    assert_equal("fizzbuzz", result[29])
+    assert_equal("fizzbuzz", result[59])
+    assert_equal("fizzbuzz", result[89])
   end
   	
 end
